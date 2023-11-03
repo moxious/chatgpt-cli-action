7c7eaf40097a69012802e91ea12a8c8bcc6599fe
moxious <david.allen@grafana.com>
2023-11-03 10:48:54 -0400

    mod

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+TYPE=$1
+URL=$2
+PROMPT=$3
 
 echo "Hello $1 / $2"
+
 # gh issue view https://whatever
-gh "$2" view "$1"
-echo "Tell me a short story about a turtle with moxie" | chatgpt-cli | tee story.txt
-echo output=\"`cat story.txt`\" >> $GITHUB_OUTPUT
+gh "$2" view "$1" | tee github-context.txt
+
+echo $PROMPT > context.txt
+
+cat github-context.txt | chatgpt-cli --context context.txt | tee outputs.txt
+echo output=\"`cat outputs.txt`\" >> $GITHUB_OUTPUT
