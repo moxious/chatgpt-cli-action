1df66e408d02910ba39406e952fb2d415f98fab7
moxious <david.allen@grafana.com>
2023-11-03 10:38:26 -0400

    mod

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 echo "Hello $1 / $2"
 # gh issue view https://whatever
 gh "$2" view "$1"
-echo "Tell me a short story about a turtle" | chatgpt-cli >> $GITHUB_OUTPUT
+echo "Tell me a short story about a turtle with moxie" | chatgpt-cli >> $GITHUB_OUTPUT
